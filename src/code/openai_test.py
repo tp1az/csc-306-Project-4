8edb8e461ca0f787784800c613e582f07ea546af
Tony Piacentini <tonypiacentini@Tonys-MacBook-Air.local>
2025-03-11 14:41:53 -0400

    added code for using openai api to solve tabular data as well as the evaluation

--- src/code/openai_test.py
+++ src/code/openai_test.py
@@ -1,16 +1,14 @@
 from openai import OpenAI
 client = OpenAI(api_key= "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") 
-MESSAGE_SYSTEM_CONTENT = ("You are an assistant tasked with answering the questions asked of a given CSV in list format with pairs [[answer, value] [category, value] ... [explanation, value]] where the first index is the name of the category and the second is the value computed. You must answer in a single JSON with three fields:"
+MESSAGE_SYSTEM_CONTENT = ("You are an assistant tasked with answering the questions asked of a given CSV in JSON format. Your response should follow this guidlines {'Answer': answer}"
 + "answer: answer using information from the provided CSV only. The answer should be one of these categories: "
 + "Boolean, Valid answers include True/False, Y/N, Yes/No (all case insensitive), "
 + "Category, A value from a cell (or a substring of a cell) in the dataset, "
 + "Number, A numerical value from a cell in the dataset, which may represent a computed statistic (e.g., average, maximum, minimum), "
 + "List[category], A list containing a fixed number of categories. The expected format is: '['cat', 'dog']'. Pay attention to the wording of the question to determine if uniqueness is required or if repeated values are allowed."
 + "List[number], Similar to List[category], but with numbers as its elements."
-+ "type: The type of answer you provided. Valid types include boolean, category, number, list[category], list[number]."
-+ "columns_used: list of columns from the CSV used to get the answer."
-+ "explanation: A short explanation on why you gave that answer if the answer is False, give the correct answer and how it is different from the question."
-+ "Requirements: Only respond with the list .")
++ "Requirements: Only respond with the JSON, do not add any additional text only use respond in the JSON format."
+)
 def response_test(question:str, context:str, model:str = "gpt-4o-mini"):
     response = client.chat.completions.create(
         model=model,
