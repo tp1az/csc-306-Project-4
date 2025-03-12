5d765bf62d0c26d661a48a0b3a6170e859b37497
Tony Piacentini <tonypiacentini@tonys-air.union.edu>
2025-03-12 16:56:34 -0400

    this is to push the updated working evaluation of the models

--- src/code/openai_test.py
+++ src/code/openai_test.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key= "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") 
+client = OpenAI(api_key= "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") 
 MESSAGE_SYSTEM_CONTENT = ("You are an assistant tasked with answering the questions asked of a given CSV in JSON format. Your response should follow this guidlines {'Answer': answer}"
 + "answer: answer using information from the provided CSV only. The answer should be one of these categories: "
 + "Boolean, Valid answers include True/False, Y/N, Yes/No (all case insensitive), "
@@ -7,9 +7,9 @@ MESSAGE_SYSTEM_CONTENT = ("You are an assistant tasked with answering the questi
 + "Number, A numerical value from a cell in the dataset, which may represent a computed statistic (e.g., average, maximum, minimum), "
 + "List[category], A list containing a fixed number of categories. The expected format is: '['cat', 'dog']'. Pay attention to the wording of the question to determine if uniqueness is required or if repeated values are allowed."
 + "List[number], Similar to List[category], but with numbers as its elements."
-+ "Requirements: Only respond with the JSON, do not add any additional text only use respond in the JSON format."
++ "Requirements: Only respond with the JSON, do not add any additional text only use respond in the JSON format. Respond with {Answer : your response}, if your response is a list do {Answer : list}"
 )
-def response_test(question:str, context:str, model:str = "gpt-4o-mini"):
+def response_test(question:str, context:str, model:str = "gpt-3.5-turbo"):
     response = client.chat.completions.create(
         model=model,
         messages=[
